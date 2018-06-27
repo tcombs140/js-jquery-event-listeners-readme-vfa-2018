ff9245924c559023a5155bcdf71244ef1dfeb7d1
Taylor Combs <t.combs140@gmail.com>
2018-06-27 15:35:27 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,29 @@
 //define functions here
+function getIt() {
+  $('p').on('click',function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt(){
+  $('img').on('load',function(){
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt(){
+  $('input').on('keydown',function(key){
+    if(key.which == 71){
+      alert('G was pressed');
+    }
+  });
+}
+
+function submitIt() {
+  $(document).on('submit', function(){
+    alert('Your form is going to be submitted now.');
+  });
+}
 
 $(document).ready(function(){
 
